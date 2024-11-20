22154c994fd24068e0aac2e5d24ec74a4d0a287f
alicja <alicja.charuza@gmail.com>
2024-11-20 13:52:55 +0100

    check preprocessing time for all files

--- backend/recycling_app/preprocessing/Preprocessing.py
+++ backend/recycling_app/preprocessing/Preprocessing.py
@@ -3,7 +3,7 @@ from PIL import Image
 import torch
 
 
-class Preprocessing:
+class Preprocessor:
 
     def __init__(self) -> None:
         self.transform = v2.Compose(
@@ -12,8 +12,8 @@ class Preprocessing:
                 v2.ToDtype(torch.uint8, scale=True),
                 v2.Grayscale(num_output_channels=3),
                 v2.GaussianBlur(kernel_size=5, sigma=(0.1, 2.0)),
-                v2.RandomResizedCrop(size=(224, 224), scale=(0.3, 1), antialias=True),
                 v2.RandomRotation(degrees=(0, 360)),
+                v2.RandomResizedCrop(size=(224, 224), scale=(0.3, 1), antialias=True),
                 v2.ToDtype(torch.float32, scale=True),
                 v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             ]
