204c9b1e9d0ea4d3a7e5914cb6fc150f32e2ae50
alicja <alicja.charuza@gmail.com>
2024-11-24 15:35:40 +0100

    prepare model_experiments3

--- backend/recycling_app/preprocessing/loading_data.py
+++ backend/recycling_app/preprocessing/loading_data.py
@@ -19,7 +19,7 @@ def create_data_loaders(
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-    return train_loader, val_loader, test_loader
+    return train_loader, val_loader, test_loader, dataset
 
 
 def _calculate_sizes(dataset: datasets.ImageFolder, split_ratio: float) -> tuple[int, int, int]:
