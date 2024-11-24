942e5e23d28a741de368bc3af14400afa436d6bf
alicja <alicja.charuza@gmail.com>
2024-11-24 14:08:27 +0100

    remove preprocessing from waste_prediction endpoint

--- backend/recycling_app/api/routers/waste_prediction.py
+++ backend/recycling_app/api/routers/waste_prediction.py
@@ -1,13 +1,11 @@
 from fastapi import APIRouter, File, UploadFile
 from fastapi.responses import JSONResponse
 from starlette import status
-from recycling_app.preprocessing.Preprocessing import Preprocessor
-import torch
 from io import BytesIO
 from recycling_app.api.constants import FILE_TYPES
+from PIL import Image
 
 router = APIRouter()
-img_preprocessor = Preprocessor()
 
 
 @router.post("/waste_prediction")
@@ -18,22 +16,23 @@ async def waste_prediction(file: UploadFile = File(...)):
             content={"message": "Unsupported file type"},
         )
     try:
-        raw_img = BytesIO(await file.read())
+        img = BytesIO(await file.read())
     except IOError as e:
         return JSONResponse(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             content={"message": "Error reading image", "details": str(e)},
         )
-    preprocessed_img = img_preprocessor(raw_img)
-    model_response = mock_model_response(preprocessed_img)
+    img = Image.open(img)
+    # showing image to prove that it's been sent
+    img.show()
+    model_response = mock_model_response(img)
 
-    # returning tensor_shape to check if the image was preprocessed correctly
     return JSONResponse(
         status_code=status.HTTP_200_OK,
-        content={"prediction": model_response, "tensor_shape": preprocessed_img.shape},
+        content={"prediction": model_response},
     )
 
 
 # mock_model_response is used to simulate the response of a machine learning model.
-def mock_model_response(preprocessed_img: torch.Tensor) -> str:
+def mock_model_response(img: Image.Image) -> str:
     return "paper"
