be92aabc66fbf9febde04e370e2b8d17c38cfe4a
alicja <alicja.charuza@gmail.com>
2024-11-27 10:16:37 +0100

    add diffferent transformations for test and train

--- backend/recycling_app/preprocessing/preprocessing.py
+++ backend/recycling_app/preprocessing/preprocessing.py
@@ -5,7 +5,7 @@ import torch
 class Preprocessor:
 
     def __init__(self) -> None:
-        self.transform = v2.Compose(
+        self.transform_train = v2.Compose(
             [
                 v2.ToImage(),
                 v2.ToDtype(torch.uint8, scale=True),
@@ -13,6 +13,15 @@ class Preprocessor:
                 v2.GaussianBlur(kernel_size=5, sigma=(0.1, 2.0)),
                 v2.RandomRotation(degrees=(0, 360)),
                 v2.RandomResizedCrop(size=(224, 224), scale=(0.3, 1), antialias=True),
+                v2.ToDtype(torch.float32, scale=True),
+                v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+        )
+        self.transform_test = v2.Compose(
+            [
+                v2.ToImage(),
+                v2.ToDtype(torch.uint8, scale=True),
+                v2.Grayscale(num_output_channels=3),
                 v2.ToDtype(torch.float32, scale=True),
                 v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             ]
