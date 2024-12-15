fa1d4d6fe83c352fad5bc7ba665cbd1d5fa82ec9
alicja <alicja.charuza@gmail.com>
2024-12-15 19:17:17 +0100

    add support for images with an alpha channel

--- backend/recycling_app/preprocessing/preprocessing.py
+++ backend/recycling_app/preprocessing/preprocessing.py
@@ -2,6 +2,7 @@ from torchvision.transforms import v2
 import torch
 from typing import Union
 from io import BytesIO
+from PIL import Image
 
 
 class Preprocessor:
@@ -10,6 +11,7 @@ class Preprocessor:
         self.transform_train = v2.Compose(
             [
                 v2.ToImage(),
+                v2.Lambda(lambda x: self.remove_alpha(x)),
                 v2.ToDtype(torch.uint8, scale=True),
                 v2.Grayscale(num_output_channels=3),
                 v2.GaussianBlur(kernel_size=5, sigma=(0.1, 2.0)),
@@ -22,6 +24,7 @@ class Preprocessor:
         self.transform_test = v2.Compose(
             [
                 v2.ToImage(),
+                v2.Lambda(lambda x: self.remove_alpha(x)),
                 v2.ToDtype(torch.uint8, scale=True),
                 v2.Grayscale(num_output_channels=3),
                 v2.Resize(size=(224, 224), antialias=True),
@@ -29,3 +32,11 @@ class Preprocessor:
                 v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             ]
         )
+    
+    def remove_alpha(self, img, background_color=(255, 255, 255)):
+        if img.mode in ("RGBA", "LA") or (img.mode == "P" and "transparency" in img.info):
+            background = Image.new("RGB", img.size, background_color)
+            background.paste(img, mask=img.getchannel("A"))  # Nakładanie z kanałem alfa
+            return background
+        else:
+            return img.convert("RGB")
